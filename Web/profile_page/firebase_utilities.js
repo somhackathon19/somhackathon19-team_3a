819ee72eba95cb15f2dc4bfd378ae1d481d0acce
Joanp131 <Joanp131@users.noreply.github.com>
2019-02-09 23:41:32 +0100

    d

--- Web/profile_page/firebase_utilities.js
+++ Web/profile_page/firebase_utilities.js
@@ -1,9 +1,23 @@
-var config = {
-    apiKey: "AIzaSyBz3ggHudt1W9wZnzzMAQFP7sR4yU5skXI",
-    authDomain: "users-88e93.firebaseapp.com",
-    databaseURL: "https://users-88e93.firebaseio.com",
-    projectId: "users-88e93",
-    storageBucket: "users-88e93.appspot.com",
-    messagingSenderId: "110252542734"
-};
-firebase.initializeApp(config);
+let a;
+
+window.onload = function() {
+    var config = {
+        apiKey: "AIzaSyBz3ggHudt1W9wZnzzMAQFP7sR4yU5skXI",
+        authDomain: "users-88e93.firebaseapp.com",
+        databaseURL: "https://users-88e93.firebaseio.com",
+        projectId: "users-88e93",
+        storageBucket: "users-88e93.appspot.com",
+        messagingSenderId: "110252542734"
+    };
+    firebase.initializeApp(config);
+
+    fetch("")
+}
+
+function getData() {
+    firebase.database().ref('users').on("value", data => {
+        let users = data.val()
+
+        console.log(users.age)
+    }, err => console.error(err))
+}
