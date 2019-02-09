db61958d39a073edbaeba4847be023169316ebcd
Joanp131 <Joanp131@users.noreply.github.com>
2019-02-09 23:22:23 +0100

    f

---
+++ Web/profile_page/firebase_utilities.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyBz3ggHudt1W9wZnzzMAQFP7sR4yU5skXI",
+    authDomain: "users-88e93.firebaseapp.com",
+    databaseURL: "https://users-88e93.firebaseio.com",
+    projectId: "users-88e93",
+    storageBucket: "users-88e93.appspot.com",
+    messagingSenderId: "110252542734"
+};
+firebase.initializeApp(config);
