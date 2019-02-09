64f220efd76a22357c08e8c60f5ac4f3cc48258b
Carlos Chuan <carlos.chuan@mataro.epiaedu.cat>
2019-02-09 22:26:44 +0100

    j

--- Web/home/sketch.js
+++ Web/home/sketch.js
@@ -1,3 +1,45 @@
 function print(a) {
     console.log(a);
 }
+
+let checked = []
+
+function chech(id) {
+    console.log(typeof id)
+    let a = document.getElementById("example-number-input")
+    switch (id) {
+        case "1":
+            a.setAttribute("max", "5")
+            a.setAttribute("value", "0")
+            console.log(id)
+            break;
+        case "2":
+            a.setAttribute("max", "7")
+            a.setAttribute("value", "5")
+            break;
+        case "3":
+            a.setAttribute("max", "15")
+            a.setAttribute("value", "7")
+            break;
+        case "4":
+            a.setAttribute("max", "22")
+            a.setAttribute("value", "15")
+            break;
+        case "5":
+            a.setAttribute("max", "30")
+            a.setAttribute("value", "22")
+            break;
+        case "6":
+            a.setAttribute("max", "37")
+            a.setAttribute("value", "30")
+            break;
+        case "7":
+            a.setAttribute("max", "45")
+            a.setAttribute("value", "37")
+            break;
+        default:
+            a.setAttribute("max", "0")
+            break;
+        
+    }
+}
