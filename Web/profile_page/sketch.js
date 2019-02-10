363bba222f9f5050e95522d4826189e42fb45d58
Carlos Chuan <carlos.chuan@mataro.epiaedu.cat>
2019-02-10 11:39:59 +0100

    s

--- Web/profile_page/sketch.js
+++ Web/profile_page/sketch.js
@@ -1,24 +1,17 @@
 
 
 var User1 = {
-    "name": "Carlos",
+    "name": "Victor",
     "surname": "Chuan Lleonart",
     "age": "17",
     "number": "686 414 216",
     "email": "chuancho22@gmail.com",
-    "password": "alvaricoque"
-}
-console.log(User1);
-window.onload = function(){
-    console.log(User1);
-    document.getElementById('firstName').textContent = User1.name;
-    document.getElementById('lastName').textContent = User1.surname;
-    document.getElementById('age').textContent = User1.age;
-    document.getElementById('tlfNumber').textContent = User1.number;
+    "password": "alvaricoque",
+    "coins": "25",
 }
 
-function hola() {
-    console.log(User1);
+console.log(User1);
+window.onload = function(){
     document.getElementById('firstName').textContent = User1.name;
     document.getElementById('lastName').textContent = User1.surname;
     document.getElementById('age').textContent = User1.age;
