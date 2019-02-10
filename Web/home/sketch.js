1e26a0d5349b14e3dad275350ddd60dc5b74bbeb
Joanp131 <Joanp131@users.noreply.github.com>
2019-02-10 02:41:43 +0100

    yes

--- Web/home/sketch.js
+++ Web/home/sketch.js
@@ -43,3 +43,100 @@ function chech(id) {
         
     }
 }
+
+function submit() {
+    let a = {};
+    a.title = document.getElementById("usr").value;
+    a.desc = document.getElementById("comment").value;
+    a.time = document.getElementById("example-datetime-local-input").value
+    a.imgLink = document.getElementById("customFileLang").value
+    a.tema = document.getElementById("tema").value
+    a.long = document.getElementById("event_length").name
+    a.value = document.getElementById("example-number-input").value
+
+    createEvent(a);
+
+
+    console.log(a)
+}
+
+function createEvent(obj) {
+    done();
+    let div1 = document.createElement("div")
+    div1.classList.add("card")
+
+    let div2 = document.createElement("div")
+    div2.classList.add("card-body")
+    div2.classList.add("navbar")
+
+    let a = document.createElement("a", {href: `${obj.imgLink}`})
+    a.classList.add("nav")
+    a.classList.add("navbar-brand")
+    a.classList.add("pull-sm-right")
+
+    let img = document.createElement("img")
+    img.setAttribute("src", `${obj.imgLink}`)
+    img.setAttribute("class", "rounded")
+    img.setAttribute("alt", "Logo")
+    img.setAttribute("style", "width: 50px;")
+
+    let ul = document.createElement("ul")
+    ul.setAttribute("class", "navbar-nav")
+    ul.setAttribute("class", "mr-auto")
+
+    let li1 = document.createElement("li")
+    li1.setAttribute("class", "nav-item")
+    li1.setAttribute("class", "avtive")
+    li1.innerText = `${obj.title}`;
+
+    let li2 = document.createElement("li", {class:"nav-item active"})
+    li2.setAttribute("class", "nav-item")
+    li2.setAttribute("class", "active")
+
+    let a2 = document.createElement("a")
+    a2.setAttribute("class", "nav-link")
+    a2.setAttribute("href", "#")
+    a2.innerText = `${obj.value} ₪`
+
+    let span = document.createElement("span")
+    span.setAttribute("class", "sr-only")
+    span.innerHTML = "(current)"
+
+    let div3 = document.createElement("div", {class:"card-footer", type:"button", "data-toggle":"modal", "data-target":"#myModal1", style:"cursor: pointer"})
+    div3.setAttribute("class", "card-footer")
+    div3.setAttribute("type", "button")
+    div3.setAttribute("data-toggle", "modal")
+    div3.setAttribute("data-target", "someid")
+    div3.setAttribute("style", "cursor: pointer")
+
+    let father = document.getElementById("cards_container")
+    father.appendChild(div1)
+    div1.appendChild(div2)
+    div2.appendChild(a)
+    a.appendChild(img)
+    div2.appendChild(ul)
+    ul.appendChild(li1)
+    ul.appendChild(li2)
+    li2.appendChild(a2)
+    a2.appendChild(span)
+    father.insertBefore(div1, document.getElementById("card1"))
+    father.appendChild(div3)
+}
+
+function done()  {
+    document.getElementById("comment").style.display = "none";
+    document.getElementById("example-datetime-local-input").style.display = "none"
+    document.getElementById("customFileLang").style.display = "none"
+    document.getElementById("tema").style.display = "none"
+    document.getElementById("event_length").style.display = "none"
+    document.getElementById("example-number-input").style.display = "none"
+    document.getElementById("jaja1").style.display = "none"
+    document.getElementById("jaja2").style.display = "none"
+    document.getElementById("jaja3").style.display = "none"
+    document.getElementById("jaja4").style.display = "none"
+    document.getElementById("jaja5").style.display = "none"
+    document.getElementById("jaja6").style.display = "none"
+    document.getElementById("jaja7").style.display = "none"
+    document.getElementById("jaja8").style.display = "none"
+    document.getElementById("bien").innerText = "Event creat correctament!"
+}
--- Web/profile_page/firebase_utilities.js
+++ Web/profile_page/firebase_utilities.js
@@ -1,23 +1,24 @@
-let a;
+let a = [];
+var config = {
+    apiKey: "AIzaSyBz3ggHudt1W9wZnzzMAQFP7sR4yU5skXI",
+    authDomain: "users-88e93.firebaseapp.com",
+    databaseURL: "https://users-88e93.firebaseio.com",
+    projectId: "users-88e93",
+    storageBucket: "users-88e93.appspot.com",
+    messagingSenderId: "110252542734"
+};
+firebase.initializeApp(config);
 
-window.onload = function() {
-    var config = {
-        apiKey: "AIzaSyBz3ggHudt1W9wZnzzMAQFP7sR4yU5skXI",
-        authDomain: "users-88e93.firebaseapp.com",
-        databaseURL: "https://users-88e93.firebaseio.com",
-        projectId: "users-88e93",
-        storageBucket: "users-88e93.appspot.com",
-        messagingSenderId: "110252542734"
-    };
-    firebase.initializeApp(config);
 
-    fetch("")
-}
-
-function getData() {
-    firebase.database().ref('users').on("value", data => {
-        let users = data.val()
-
-        console.log(users.age)
+function retrieveData() {
+    firebase.database().ref('users-88e93').on("value", data => {
+      let users = data.val()
+  
+      a.push(users.age)
+      a.push(users.email)
+      a.push(users.interessos)
+      a.push(users.perfil)
     }, err => console.error(err))
-}
+  
+    console.warn("Data successfully retrieved from Firebase App!")
+  }
